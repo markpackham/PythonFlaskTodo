8f8b75f286733fef52a95b81559e179b461f6085
mark <markpackham1@gmail.com>
2021-12-21 12:55:32 +0000

    add model
    https://youtu.be/Z1RJmh_OqeA?t=1182

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    # for every new element create a Task with an id
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
